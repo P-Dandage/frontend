20a706d8b5e112876acd6e07b6fdda6522911086
P-Dandage <prathamesh@prathameshs-MacBook-Air.local>
2025-03-29 20:53:26 +0530

    final

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-   
-    <div className='navbar'>
-        <div className=' nav'>
-          <img src='./logo_2.png'></img>
-        <div className='nav_menu'>
-            <a href='#'>Home</a>
-            <a href='#'>About</a>
-            <a href='#'>Contact US</a>
-            </div>
-      
-      <div className='nav_right'>
-      <button > Get Started <span class="material-symbols-outlined">
-arrow_forward
-</span></button>
-      </div>
+    <div className="navbar">
+      <div className="nav">
+        <img src="./logo_2.png" alt="Logo" />
+
+        <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          <span className={isOpen ? "bar open" : "bar"}></span>
+          <span className={isOpen ? "bar open" : "bar"}></span>
+          <span className={isOpen ? "bar open" : "bar"}></span>
+        </div>
+        <div className={`nav_menu ${isOpen ? "open" : ""}`}>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact Us</a>
+        </div>
+        <div className="nav_right">
+          <button>
+            Get Started <span className="material-symbols-outlined">arrow_forward</span>
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
